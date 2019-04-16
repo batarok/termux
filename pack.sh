d8b73099f0d4c3445242e235b8df2bb0382686ff
beginer <taufik.11090@gmail.com>
2019-04-16 18:16:01 +0700

    Update pack.sh

--- pack.sh
+++ pack.sh
@@ -4,6 +4,12 @@ termux-setup-storage
 apt update && apt upgrade -y
 apt install -y git nano wget curl php
 
+cp -R "$HOME/.termux/termux" "$HOME/.termux"
+
+
+termux-reload-settings
+
+
 echo "Done proses \n"
 echo "Please restart Termux app..."
 exit
