0cc96cf14018480dfe2dc803b47f6de3399acb53
beginer <taufik.11090@gmail.com>
2019-04-16 18:17:49 +0700

    Update pack.sh

--- pack.sh
+++ pack.sh
@@ -4,7 +4,7 @@ termux-setup-storage
 apt update && apt upgrade -y
 apt install -y git nano wget curl php
 
-cp -R "$HOME/.termux/termux" "$HOME/.termux"
+cp -R "$HOME/.termux/termux" "$HOME/.termux/"
 
 
 termux-reload-settings
